e8ce866d05b6d3820133d3db80090f3ee6443ad6
Ubuntu <neo@neofung-ubuntu.neofung-ubuntu.h7.internal.cloudapp.net>
2016-07-12 04:11:37 +0000

    use chacha20 as encrypt method

---
+++ README.md
@@ -0,0 +1,3 @@
+# A script to start a docker for shadowsocks-libev
+
+https://github.com/shadowsocks/shadowsocks-libev/tree/master/docker/ubuntu
--- start.sh
+++ start.sh
@@ -1,9 +1,17 @@
-PASSWORD=Pass_word
-PORT=1984
-NAME=shadowsocks
+#https://github.com/shadowsocks/shadowsocks-libev/tree/master/docker/ubuntu
 
+PASSWORD="Pass_word"
+PORT="1984"
+NAME="shadowsocks"
+METHOD="chacha20"
+
+echo install docker
 apt-get install docker.io
+
+echo stop and remove shadowsocks instance
 docker stop shadowsocks
 docker rm shadowsocks
-docker run --name $NAME --detach --publish $PORT:8338 --publish $PORT:8838/udp shadowsocks/shadowsocks-libev -k $PASSWORD -m aes-256-cfb --fast-open -v
+
+echo run the image
+docker run --name $NAME --detach --publish $PORT:8338 --publish $PORT:8838/udp shadowsocks/shadowsocks-libev -k $PASSWORD --fast-open -v -m $METHOD
 docker ps -al
